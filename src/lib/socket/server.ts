dd9dd887c4dd12d15f1cdd75f330dc7ac9ed78de
Ryan Jacobs <rybread42@gmail.com>
2020-10-22 12:16:04 -0700

    Create random name when user joins room

--- src/lib/socket/server.ts
+++ src/lib/socket/server.ts
@@ -3,7 +3,7 @@ import { Server } from 'http';
 //import { isPayToScriptHash } from '@bitauth/libauth';
 // import { useFourStackItems } from '@bitauth/libauth';
 import io from 'socket.io';
-// import { adjectives, animals, Config, uniqueNamesGenerator } from 'unique-names-generator';
+import { adjectives, animals, Config, uniqueNamesGenerator } from 'unique-names-generator';
 import { v4 as uuidv4 } from 'uuid';
 
 interface User {
@@ -24,6 +24,7 @@ export default class Manager {
     (data: Record<string, any>, ack?: (data: any) => void) => void
   ][];
   private rooms: { [roomId: string]: Room };
+  readonly nameConfig!: Config;
 
   constructor(httpServer: Server) {
     this.server = io(httpServer);
@@ -34,6 +35,13 @@ export default class Manager {
     ];
 
     this.rooms = {};
+
+    this.nameConfig = {
+      dictionaries: [adjectives, animals],
+      separator: ' ',
+      style: 'capital',
+      length: 2,
+    };
   }
 
   listen() {
@@ -55,7 +63,7 @@ export default class Manager {
     console.log('socket', sock.id, 'disconnected');
   }
 
-  removeUser(sock: io.Socket): void {
+  private removeUser(sock: io.Socket): void {
     if (Object.keys(sock.rooms).length !== 0) {
       const roomId: string = sock.rooms[sock.rooms.keys[0]]; // assuming a user can only be in one room at a time
       this.rooms[roomId].users = this.rooms[roomId].users.filter((user, index, array) => {
@@ -68,7 +76,7 @@ export default class Manager {
     }
   }
 
-  newHost(roomId: string): void {
+  private newHost(roomId: string): void {
     const room: Room = this.rooms[roomId];
     const randIdx: number = Math.floor(Math.random() * room.users.length);
     console.log("Chose user", room.users[randIdx].userName, "as new host.")
@@ -87,17 +95,39 @@ export default class Manager {
         roomId,
       })
     );
-
+    this.rooms[roomId] = {
+      users: [],
+      host: "",
+    }
     this.createNewUser(sock, roomId);
   }
 
-  createNewUser(sock: io.Socket, roomId: number): void {
+  private createNewUser(sock: io.Socket, roomId: string): void {
     const newUser: User = {
       userId: sock.id,
-      userName: "User" + this.rooms[roomId].users.length,
+      userName: this.generateName(roomId),
       image: "newImage",
     }
 
     this.rooms[roomId].users.push(newUser); 
+  }
+
+  private generateName(roomId: string): string {
+    const set: Set<string> = this.getCurrentUsernames(roomId);
+    let randName: string;
+    do {
+      randName = uniqueNamesGenerator(this.nameConfig);
+    } while (set.has(randName));
+    console.log("New name:", randName);
+    return randName;
+  }
+
+  private getCurrentUsernames(roomId: string): Set<string> {
+    const users: User[] = this.rooms[roomId].users;
+    const usernames: string[] = [];
+    for (const user of users) {
+      usernames.push(user.userName);
+    }
+    return new Set(usernames);
   }
 }
