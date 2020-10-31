b4a5d2a673e7e8a806577ceeb6f73283149efb54
Brandon Paiz <paiz.brandon@gmail.com>
2020-10-31 15:50:25 -0700

    Synchronize party videos

--- src/lib/socket/events.ts
+++ src/lib/socket/events.ts
@@ -4,4 +4,12 @@ export enum SocketEvents {
   SEND_MESSAGE = 'send_message',
   NEW_MESSAGE = 'new_message',
   GET_MESSAGES = 'get_messages',
+  VIDEO_EVENT = 'video_event',
+}
+
+export enum VideoSocketEvents {
+  VIDEO_PLAY = 'video_play',
+  VIDEO_PAUSE = 'video_pause',
+  VIDEO_SEEKED = 'video_seeked',
+  VIDEO_PROGRESS = 'video_progress',
 }
